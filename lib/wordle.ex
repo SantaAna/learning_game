1fa8abb40a1653e55483d47c69b44b7d9e87a9a3
Patrick <patrick@pop-os.localdomain>
2023-06-26 14:23:20 -0700

    wordle kind of working

--- lib/wordle.ex
+++ lib/wordle.ex
@@ -1,15 +1,36 @@
 defmodule Wordle do
-  @spec feedback(String.t() , String.t()) :: list()
+  @spec feedback(String.t(), String.t()) :: list()
   def feedback(target_word, guessed_word) do
-    Enum.zip([String.graphemes(target_word), String.graphemes(guessed_word)])
-    |> Enum.map(fn
-      {same, same} -> :green
-      {_tar_char, guess_char} ->
-      if guess_char in String.graphemes(target_word) do
-        :yellow
-      else
-        :gray
-      end
+    target_counts = count_chars(target_word)
+
+    {green_matches, remaining_counts} =
+      Enum.zip([String.graphemes(target_word), String.graphemes(guessed_word)])
+      |> Enum.reduce({[], target_counts}, fn {target_char, guess_char}, {so_far, target_counts} ->
+        case {target_char, guess_char} do
+          {same, same} ->
+            {[:green | so_far], Map.update!(target_counts, same, &(&1 - 1))}
+
+          {_target_char, guess_char} ->
+            {[guess_char | so_far], target_counts}
+        end
+      end)
+
+    Enum.reduce(green_matches, {[], remaining_counts}, fn
+      :green, {so_far, remaining_counts} ->
+        {[:green | so_far], remaining_counts}
+
+      char, {so_far, remaining_counts} ->
+        case remaining_counts[char] do
+          nil -> {[:gray | so_far], remaining_counts}
+          0 -> {[:gray | so_far], remaining_counts}
+          _ -> {[:yellow | so_far], Map.update!(remaining_counts, char, &(&1 - 1))}
+        end
     end)
+    |> elem(0)
+  end
+
+  def count_chars(string) do
+    String.graphemes(string)
+    |> Enum.frequencies()
   end
 end
--- test/wordle_test.exs
+++ test/wordle_test.exs
@@ -6,10 +6,37 @@ defmodule WordleTest do
     test "returns all green for exact match" do
       assert Wordle.feedback("race", "race") === [:green, :green, :green, :green]
     end
-    test "returns partial green for inexact match" do
-      assert Wordle.feedback("pace", "race") == [:gray, :green, :green, :green]
-    end
-    test "returns yellow for out of place character"
 
+    test "returns partial green for inexact match" do
+      assert Wordle.feedback("pace", "race") === [:gray, :green, :green, :green]
+    end
+
+    test "returns yellow for out of place character" do
+      assert Wordle.feedback("tree", "feet") === [:gray, :yellow, :green, :yellow]
+    end
+
+    test "returns gray for out of place character which cannot appear again in word" do
+      assert Wordle.feedback("treat", "fleet") === [:gray, :gray, :green, :gray, :green]
+    end
+
+    # test "returns all gray for 0 character matches" do
+    #   assert Wordle.feedback("jimbob", "ranchy")
+    # end
+
+    # test "returns 3 yellow for out of place characters" do
+    #   assert Wordle.feedback("shrimp", "blimpy")
+    # end
+
+    test "returns 3 yellow for out of place characters" do
+      assert Wordle.feedback("glimpse", "sssssss") == [
+               :gray,
+               :gray,
+               :gray,
+               :gray,
+               :gray,
+               :green,
+               :gray
+             ]
+    end
   end
 end
