dbc71c0d9732d2f37504be253c6703fa6bb86ec4
Patrick <patrick@pop-os.localdomain>
2023-06-28 13:50:45 -0700

    updated display

---
+++ lib/games/rock_paper_scissors/display.ex
@@ -0,0 +1,59 @@
+defmodule Games.RockPaperScissors.Display do
+  def welcome() do
+    Owl.Box.new("Rock Paper Scissors", padding_x: 34)
+    |> Owl.IO.puts()
+  end
+
+  def instructions() do
+    [
+      "Try to beat the computer by throwing rock, paper, or scissors"
+    ]
+    |> Owl.Box.new(title: "Rules", max_width: 76)
+    |> Owl.IO.puts()
+  end
+
+  def get_user_input() do
+    Owl.IO.select(["rock", "paper", "scissors"],
+      render_as: fn name ->
+        [Owl.Data.tag(name, :cyan)]
+      end
+    )
+  end
+
+  def draw() do
+    Owl.Data.tag("Draw", :yellow)
+    |> Owl.Box.new(padding_x: 34, border_tag: :yellow)
+    |> Owl.IO.puts()
+
+    Process.sleep(1000)
+    Games.Menu.display_main_menu()
+  end
+
+  def defeat() do
+    Owl.Data.tag("You Lose", :red)
+    |> Owl.Box.new(padding_x: 34, border_tag: :red)
+    |> Owl.IO.puts()
+
+    Process.sleep(1000)
+    Games.Menu.display_main_menu()
+  end
+
+  def victory() do
+    Owl.Data.tag("You Win!", :green)
+    |> Owl.Box.new(padding_x: 34, border_tag: :green)
+    |> Owl.IO.puts()
+
+    Process.sleep(1000)
+    Games.Menu.display_main_menu()
+  end
+
+  def display_feed_back(computer_play, human_play) do
+    [
+      Owl.Data.tag("You play #{human_play}", :green),
+      "\n",
+      Owl.Data.tag("Computer plays #{computer_play}", :red)
+    ]
+    |> Owl.IO.puts()
+  end
+end
+
