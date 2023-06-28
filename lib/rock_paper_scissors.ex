c6d86e83c58076ea521ea8072e19694cd072b101
Patrick <patrick@pop-os.localdomain>
2023-06-27 21:05:54 -0700

    rock paper scissors typespec

--- lib/rock_paper_scissors.ex
+++ lib/rock_paper_scissors.ex
@@ -7,11 +7,13 @@ defmodule RockPaperScissors do
 
   @choices ["paper", "rock", "scissors"]
 
+  @spec play() :: :ok
   def play() do
     IO.puts("rock, paper, scissors, shoot!")
     result(computer_choice(), human_choice())
   end
-
+  
+  @spec result(String.t(), String.t()) :: :ok
   def result(comp_choice, player_choice) do
     case {@winner[{comp_choice, player_choice}], @winner[{player_choice, comp_choice}]} do
       {:winner, _} -> IO.puts("computer wins, c: #{comp_choice}, p: #{player_choice}")
@@ -19,11 +21,13 @@ defmodule RockPaperScissors do
       _ -> IO.puts("draw! c: #{comp_choice}, p: #{player_choice}")
     end
   end
-
+  
+  @spec computer_choice() :: String.t()
   def computer_choice() do
     Enum.random(@choices)
   end
 
+  @spec human_choice() :: String.t()
   def human_choice() do
     input =
       IO.gets("choose rock paper scissors: ")
