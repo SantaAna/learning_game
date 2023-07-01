38ac622d5a2ad5c2b375b81ea1d73433136a24be
Patrick <patrick@pop-os.localdomain>
2023-06-30 17:39:28 -0700

    added tests for play function

--- test/wordle_test.exs
+++ test/wordle_test.exs
@@ -1,7 +1,7 @@
 defmodule WordleTest do
   use ExUnit.Case
   alias Games.Wordle
-  # doctest Wordle
+  import Mox
 
   describe "testing feedback/2 check rounds" do
     test "returns all green for exact match" do
@@ -21,9 +21,38 @@ defmodule WordleTest do
       assert Wordle.feedback("baab", "aaaa") === [:red, :green, :green, :red]
       assert Wordle.feedback("baba", "aaab") === [:yellow, :green, :red, :yellow]
     end
-    
+
     test "returns yellow for first out of place matches, and gray if matches 'run out'" do
       assert Wordle.feedback("aabcc", "cabaa") == [:yellow, :green, :green, :yellow, :red]
     end
+  end
+
+  describe "testing play/2 function" do
+    test "displays winning message if player has won" do
+      winning_word = "dirkel"
+
+      expect(Wordle.MockDisplay, :get_user_input, 1, fn _ ->
+        winning_word
+      end)
+
+      expect(Wordle.MockDisplay, :victory, 1, fn -> nil end)
+      Wordle.play(winning_word, 1)
+      verify!(Wordle.MockDisplay)
+    end
+  end
+
+  describe "displays defeat message if player has used all guesses" do
+    expect(Wordle.MockDisplay, :defeat, 1, fn _ -> nil end)
+    Wordle.play("dirkle", 7)
+    verify!(Wordle.MockDisplay)
+  end
+
+  describe "plays an additional round if player guesses incorrectly and there are more tries" do
+    winning_word = "dirkle"
+    expect(Wordle.MockDisplay, :display_feedback, 1, fn _ -> nil end)
+    expect(Wordle.MockDisplay, :get_user_input, 1, fn _ -> "dirkly" end)
+    expect(Wordle.MockDisplay, :defeat, 1, fn _ -> nil end)
+    Wordle.play(winning_word, 6)
+    verify!(Wordle.MockDisplay)
   end
 end
