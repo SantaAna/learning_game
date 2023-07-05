030409f6ee58b17c1edf9f7c3aea297b510b049c
Patrick <patrick@pop-os.localdomain>
2023-07-04 17:58:42 -0700

    added mock for tic_tac_toe

--- config/config.exs
+++ config/config.exs
@@ -3,6 +3,7 @@ import Config
 case config_env() do
   :test ->
     config :games, wordle_display_module: Games.Wordle.MockDisplay
+    config :games, tic_tac_toe_display_module: Games.TicTacToe.MockDisplay
   :dev ->
     nil
 
